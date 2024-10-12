58e1a3f4692278f17a567b78526cc1414636b8e3
vic3008 <162638777+vic3008@users.noreply.github.com>
2024-10-13 00:17:42 +0100

    Calculator style

---
+++ Calculator project 2/index.js
@@ -0,0 +1,50 @@
+function calculator() {
+    let input1 = document.getElementById("input1").value
+    let input2 = document.getElementById("input2").value
+   let operator =document.getElementById("operator").value
+
+   //Conditional statement for input Validation//
+    var calculator
+
+    if ((input1) == '' && (input2) == '') {
+        return alert('Please kindly input a valid number or else, you go collect');
+    }
+
+    let result;
+    switch (operator) {
+       case "add":
+        result = Number(input1) + Number(input2);
+        break;
+    }
+    switch (operator) {
+       case "add":
+        result = Number(input1) + Number(input2);
+        break;
+    }
+    switch (operator) {
+       case "subtract":
+        result = Number(input1) - Number(input2);
+        break;
+    }
+    switch (operator) {
+       case "multiply":
+        result = Number(input1) * Number(input2);
+        break;
+    }
+    switch (operator) {
+       case "divide":
+        result = Number(input1) / Number(input2);
+        break;
+    }
+    switch (operator) {
+       case "exponential":
+        result = Number(input1) ** Number(input2);
+        break;
+    }
+    let resultTag = document.getElementById("result");
+    resultTag.innerHTML = `Result: ${result}`
+}
+
+
+
+// alert("Result": + result)
